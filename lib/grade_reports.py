0af714e63e26da63b7341190e33529a93a5826c0
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-01-30 12:57:43 -0700

    Comment

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+# DEV BABY
 def create_grade_report(student_grades):
-    with open('lib/reports/grade_report.txt', 'w') as gr:
+    with open('lib/reports/grade_report.txt0', 'w') as gr:
         for grade in student_grades:
             # add '\n' to write grades on separate lines
             gr.write(grade + '\n')
